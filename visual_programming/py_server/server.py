634c58c74cabbc39c24c9e24425361be4a203601
Matvey Gotfrid <matveygot@yandex.ru>
2024-03-28 16:11:19 +0700

    Add test server

---
+++ visual_programming/py_server/server.py
@@ -0,0 +1,17 @@
+import socket
+
+SERVER = socket.socket()
+SERVER_ADDR = ('localhost', 3000)
+SERVER.bind(SERVER_ADDR)
+
+SERVER.listen(1)
+print(f"Server in ON with address: {SERVER_ADDR}")
+
+CLIENT, CLIENT_ADDR = SERVER.accept()
+print(f"Connection established with: {CLIENT_ADDR}")
+
+data = CLIENT.recv(1024)
+print(f"Get data from user: {data}")
+
+CLIENT.close()
+SERVER.close()
